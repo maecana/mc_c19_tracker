3bf8d072b7fe96f0da4b2e73b88aee67a408c753
Mae Cana <mae.cana.58@gmail.com>
2020-06-08 02:27:48 +0800

    Styling
    1. Changed google maps style to dark mode
    2. Added map-style.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let map;
 initMap = () => {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 12.8797, lng: 121.7740},
-        zoom: 8
+        zoom: 8,
+        styles: mapStyle
     });
 }
