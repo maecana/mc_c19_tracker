5fc85793ad13304e5a8eab698fff499ae77bfbfc
Mae Cana <mae.cana.58@gmail.com>
2020-06-08 02:19:29 +0800

    Initial Commit
    1. Added index.html, script.js, and style.css
    2. Shown initial state of Map with Manila as the center of the map.
    3. Added readme file

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// window.onload = () => {
+// }
+
+let map;
+initMap = () => {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 12.8797, lng: 121.7740},
+        zoom: 8
+    });
+}
