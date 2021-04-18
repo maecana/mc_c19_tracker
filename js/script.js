a4323263a68653727d74bf2788fbfd0542480256
Mae Cana <mae.cana@outlook.com>
2021-04-18 20:36:59 +0800

    Set markers

--- js/script.js
+++ js/script.js
@@ -1,10 +1,72 @@
 window.onload = () => {
+    set_table_details();
+}
 
-    let fetchCovid = fetch('https://disease.sh/v2/countries');
-    fetchCovid.then((response) => {
+
+let map;
+initMap = () => {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 12.8797, lng: 121.7740},
+        zoom: 3,
+        styles: mapStyle,   
+    });
+    
+    set_marker_details(map);
+}
+  
+
+set_marker_details = (map) => {
+    fetch_data().then((response) => {
+        return response.json();
+    }).then((jsonData) => {
+        let marker_details = [];
+
+        jsonData.map((eachCountry) => {
+            marker_details.push([
+                eachCountry.cases,
+                {
+                    lat: eachCountry.countryInfo.lat,
+                    lng: eachCountry.countryInfo.long,
+                }
+            ]);
+        });
+
+        setMarkers(map, marker_details);
+    });
+}
+
+
+setMarkers = (map, data) => {
+    for (let i = 0; i < data.length; i++) {
+        const d = data[i];
+        new google.maps.Circle({
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+            position: d[1],
+            map,
+            title: d[0],
+            zIndex: 1,
+            center: d[1],
+            radius: Math.sqrt(d[0]) * 100,
+        });
+    }
+}
+
+
+fetch_data = () => {
+    return fetch('https://disease.sh/v2/countries');
+}
+
+
+set_table_details = () => {
+    fetch_data().then((response) => {
         return response.json();
     }).then((jsonData) => {
         let data = '';
+
         jsonData.map((eachCountry) => {
             data += `<tr>
                 <td>${eachCountry.country}</td>
@@ -15,19 +77,9 @@ window.onload = () => {
         });
 
         populateTable(data);
-    }).catch((error) => {
-        console.log("ERROR", error);
     });
 }
 
-let map;
-initMap = () => {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 12.8797, lng: 121.7740},
-        zoom: 8,
-        styles: mapStyle
-    });
-}
 
 populateTable = (data) => {
     let table = document.querySelector('.table');
